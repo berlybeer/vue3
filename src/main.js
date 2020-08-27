c0e60b2f8d6e1644b8453257ef3a18fdb82ba4e8
berlybeer <b.pumacajia.cz@gmail.com>
2020-08-26 22:37:20 -0500

    Button component with template

--- src/main.js
+++ src/main.js
@@ -9,5 +9,13 @@ import router from "./router";
 
 createApp(App)
 	.component("alert", require('./components/Alert.vue').default)
+	.component('button-counter', {
+	data: function(){
+		return{
+			count: 0
+		}
+	},
+	template: '<button v-on:click="count++">You clicked me {{count}} times.</button>'
+})
   	.use(router)
   	.mount("#app");
